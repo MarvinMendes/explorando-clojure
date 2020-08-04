8e9952bd685dbe4048a6b8c294a12af12651627a
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-08-04 10:44:15 -0300

    Map e Filter

--- src/explorando_funcional/aula_quatro.clj
+++ src/explorando_funcional/aula_quatro.clj
@@ -14,3 +14,20 @@
 
 (println (update precos 0 inc))
 (println (update precos 1 inc))
+
+(defn aplica-desconto?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto         (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println "Map" (map valor-descontado precos))
+(println "Filter" (map valor-descontado (filter aplica-desconto? precos)))
+
